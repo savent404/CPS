90736ded1714867b6b7961103cb5335dadb09645
廖元垲 <Savent_gate@outlook.com>
2017-03-12 21:44:30 +0800

    cJSON test on STM32F407VGTx succed

---
+++ github_cjson/json_test_F407VG/Inc/cmd-fun.h
@@ -0,0 +1,19 @@
+#ifndef _CMD_FUN_H_
+#define _CMD_FUN_H_
+
+#include "FreeRTOS.h"
+#include "FreeRTOS_CLI.h"
+/* Public Function */
+extern void CMD_Init(void);
+
+/* Static CMD functions */
+static BaseType_t CLI_HelloWorld(char* pt, size_t size, const char *cmd);
+static BaseType_t CLI_Clear(char *pt, size_t size, const char *cmd);
+static BaseType_t CLI_CPU(char *pt, size_t size, const char *cmd);
+static BaseType_t CLI_Heap(char *pt, size_t size, const char *cmd);
+static BaseType_t CLI_ThreadList(char *pt, size_t size, const char *cmd);
+static BaseType_t CLI_JSON(char* pt, size_t size, const char *cmd);
+/* Extern CMD function */
+
+#endif
+
